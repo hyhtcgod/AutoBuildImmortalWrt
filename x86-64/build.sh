acad7a9e79542f627a1d1e3e7c06d90ec8489f7d
hyhtcgod <33592906+hyhtcgod@users.noreply.github.com>
2025-07-05 13:47:57 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -40,6 +40,7 @@ PACKAGES="$PACKAGES luci-i18n-msd_lite-zh-cn"
 PACKAGES="$PACKAGES ddns-scripts_aliyun"
 PACKAGES="$PACKAGES luci-i18n-ddns-zh-cn"
 PACKAGES="$PACKAGES luci-app-serverchan"
+PACKAGES="$PACKAGES luci-i18n-wol-zh-cn"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
